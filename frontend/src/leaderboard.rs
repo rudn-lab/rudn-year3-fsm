4c60d325d9fcbaf9d4b2cbf3c970951b8ecce2fc
Danya Generalov <danya@danya02.ru>
2024-01-05 16:28:10 +0300

    Adding sorting options to leaderboard.

--- frontend/src/leaderboard.rs
+++ frontend/src/leaderboard.rs
@@ -1,6 +1,4 @@
-use std::collections::HashSet;
-
-use api::TaskGroupLeaderboard;
+use api::{TaskGroupLeaderboard, UserAndSubmissionStats};
 use shadow_clone::shadow_clone;
 use yew::{prelude::*, suspense::use_future};
 use yew_bootstrap::component::Spinner;
@@ -31,6 +29,31 @@ pub fn leaderboard_inner(props: &LeaderboardProps) -> HtmlResult {
     let force = use_force_update();
     use_interval(move || force.force_update(), 1000);
 
+    #[derive(PartialEq, Clone, Copy)]
+    enum SortingMode {
+        AlphabeticalByName,
+        AlphabeticalByRudnId,
+        ByTotalSubmissions,
+        ByOkSubmissions,
+        ByTotalTasks,
+        ByOkTasks,
+        BySubmissionDate,
+        ByNodeCount,
+        ByLinkCount,
+    }
+
+    impl SortingMode {
+        fn is_for_task(&self) -> bool {
+            match self {
+                Self::BySubmissionDate|Self::ByLinkCount|Self::ByNodeCount => true,
+                _ => false
+            }
+        }
+    }
+
+    let selected_mode = use_state(|| SortingMode::AlphabeticalByName);
+    let selected_task = use_state(|| 0usize);
+
     prepare_popovers();
 
     let LeaderboardProps { group_slug } = props.clone();
@@ -38,13 +61,19 @@ pub fn leaderboard_inner(props: &LeaderboardProps) -> HtmlResult {
     let resp = {
         shadow_clone!(group_slug);
         use_future(|| async move {
-            reqwest::get(format!(
+            let leaderboard = reqwest::get(format!(
                 "https://fsm-api.rudn-lab.ru/tasks/{group_slug}/leaderboard",
             ))
             .await?
             .error_for_status()?
             .json::<TaskGroupLeaderboard>()
-            .await
+            .await?;
+            let users = reqwest::get(format!("https://fsm-api.rudn-lab.ru/users",))
+                .await?
+                .error_for_status()?
+                .json::<Vec<UserAndSubmissionStats>>()
+                .await?;
+            Ok::<_, reqwest::Error>((users, leaderboard))
         })?
     };
 
@@ -52,15 +81,68 @@ pub fn leaderboard_inner(props: &LeaderboardProps) -> HtmlResult {
         Err(ref failure) => {
             html!(<div class="alert alert-danger">{"Ошибка при загрузке таблицы результатов. Перезагрузите страницу. Причина: "}{failure}</div>)
         }
-        Ok(ref leaderboard) => {
-            let mut user_set = HashSet::new();
-            for task in leaderboard.tasks.iter() {
-                for submission in task.latest_submissions.iter() {
-                    user_set.insert(submission.0.clone());
+        Ok((ref users, ref leaderboard)) => {
+            let mut user_set = users.clone();
+
+            match *selected_mode {
+                SortingMode::AlphabeticalByName => {
+                    user_set.sort_by(|a, b| a.user.name.cmp(&b.user.name))
+                }
+                SortingMode::AlphabeticalByRudnId => {
+                    user_set.sort_by(|a, b| a.user.rudn_id.cmp(&b.user.rudn_id))
+                }
+                SortingMode::ByTotalSubmissions => {
+                    user_set.sort_by(|a, b| b.total_submissions.cmp(&a.total_submissions));
+                }
+                SortingMode::ByOkSubmissions => {
+                    user_set.sort_by(|a, b| b.ok_submissions.cmp(&a.ok_submissions));
+                }
+                SortingMode::ByTotalTasks => {
+                    user_set.sort_by(|a, b| b.attempted_tasks.cmp(&a.attempted_tasks));
+                }
+                SortingMode::ByOkTasks => {
+                    user_set.sort_by(|a, b| b.ok_tasks.cmp(&a.ok_tasks));
+                }
+                SortingMode::BySubmissionDate => {
+                    let mut task_num = *selected_task;
+                    if task_num >= leaderboard.tasks.len() {
+                        task_num = 0;
+                    }
+                    user_set.sort_by(|a, b| {
+                        let user_a_result = leaderboard.tasks[task_num].latest_submissions.iter().find(|v| v.0 == a.user);
+                        let user_b_result = leaderboard.tasks[task_num].latest_submissions.iter().find(|v| v.0 == b.user);
+                        match (user_a_result, user_b_result) {
+                            (None, None) => std::cmp::Ordering::Equal,
+                            (None, Some(_)) => std::cmp::Ordering::Greater,
+                            (Some(_), None) => std::cmp::Ordering::Less,
+                            (Some(a), Some(b)) => a.1.cmp(&b.1),
+                        }
+                    });
+                }
+
+                SortingMode::ByNodeCount|SortingMode::ByLinkCount => {
+                    let mut task_num = *selected_task;
+                    if task_num >= leaderboard.tasks.len() {
+                        task_num = 0;
+                    }
+                    user_set.sort_by(|a, b| {
+                        let user_a_result = leaderboard.tasks[task_num].latest_submissions.iter().find(|v| v.0 == a.user);
+                        let user_b_result = leaderboard.tasks[task_num].latest_submissions.iter().find(|v| v.0 == b.user);
+                        match (user_a_result, user_b_result) {
+                            (None, None) => std::cmp::Ordering::Equal,
+                            (None, Some(_)) => std::cmp::Ordering::Greater,
+                            (Some(_), None) => std::cmp::Ordering::Less,
+                            (Some(a), Some(b)) => {
+                                if matches!(*selected_mode, SortingMode::ByNodeCount) {
+                                    a.3.cmp(&b.3)
+                                } else {
+                                    a.4.cmp(&b.4)
+                                }
+                            },
+                        }
+                    });
                 }
             }
-            let mut user_set: Vec<_> = user_set.into_iter().collect();
-            user_set.sort_by(|a, b| a.name.cmp(&b.name));
 
             let task_heads: Html = leaderboard
                 .tasks
@@ -68,7 +150,7 @@ pub fn leaderboard_inner(props: &LeaderboardProps) -> HtmlResult {
                 .map(|t| {
                     html!(
                         <th scope="col">
-                            {&t.name}
+                            <p>{&t.name}</p>
                         </th>
                     )
                 })
@@ -76,7 +158,7 @@ pub fn leaderboard_inner(props: &LeaderboardProps) -> HtmlResult {
             let task_stats: Html = user_set.iter().map(|user| {
 
                 let task_items: Html = leaderboard.tasks.iter().map(|task| {
-                    if let Some(my_submission) = task.latest_submissions.iter().find(|v| &v.0 == user) {
+                    if let Some(my_submission) = task.latest_submissions.iter().find(|v| v.0 == user.user) {
                         html!(<td><VerdictDisplay verdict={my_submission.5.clone()} /><br />
                             {unix_time_to_locale_string(my_submission.1 as f64)}<br />{" ("}
                             {my_submission.3} // nodes
@@ -89,13 +171,111 @@ pub fn leaderboard_inner(props: &LeaderboardProps) -> HtmlResult {
                     }
                 }).collect();
 
-                html!(<tr><th scope="row">{&user.name}{" ("}{&user.rudn_id}{")"}</th>{task_items}</tr>)
+                html!(<tr>
+                        <th scope="row">
+                            <p>{&user.user.name}{" ("}{&user.user.rudn_id}{")"}</p>
+                            <p class="fw-normal">{"Посылок: "}{user.ok_submissions}{" ОК / "}{user.total_submissions}{" всего"}</p>
+                            <p class="fw-normal">{"Задач: "}{user.attempted_tasks}{" ОК / "}{user.ok_tasks}{" приступил"}</p>
+                        </th>
+                        
+                        {task_items}
+                    </tr>)
             }).collect();
+
+            let sorting_options = {
+                let on_outer_select = {
+                    shadow_clone!(selected_mode);
+                    Callback::from(move |ev: InputEvent| {
+                        let el: web_sys::HtmlSelectElement = ev.target_dyn_into().unwrap();
+                        match el.value().as_str() {
+                            "alpha-name" => {
+                                selected_mode.set(SortingMode::AlphabeticalByName);
+                            }
+                            "alpha-rudnid" => {
+                                selected_mode.set(SortingMode::AlphabeticalByRudnId);
+                            }
+                            "total-submissions" => {
+                                selected_mode.set(SortingMode::ByTotalSubmissions);
+                            }
+                            "ok-submissions" => {
+                                selected_mode.set(SortingMode::ByOkSubmissions);
+                            }
+                            "total-tasks" => {
+                                selected_mode.set(SortingMode::ByTotalTasks);
+                            }
+                            "ok-tasks" => {
+                                selected_mode.set(SortingMode::ByOkTasks);
+                            }
+                            "submission-date" => {
+                                selected_mode.set(SortingMode::BySubmissionDate);
+                            }
+                            "node-count" => {
+                                selected_mode.set(SortingMode::ByNodeCount);
+                            }
+                            "link-count" => {
+                                selected_mode.set(SortingMode::ByLinkCount);
+                            }
+
+
+
+                            what => {
+                                log::error!("Unexpected value for outer select: {what:?}");
+                            }
+                        }
+                    })
+                };
+                let outer_select = html!(
+                    <select oninput={on_outer_select}>
+                        <option value="alpha-name" selected={matches!(*selected_mode, SortingMode::AlphabeticalByName)}>{"По алфавиту по имени"}</option>
+                        <option value="alpha-rudnid" selected={matches!(*selected_mode, SortingMode::AlphabeticalByRudnId)}>{"По алфавиту по номеру билета"}</option>
+                        <option value="total-submissions" selected={matches!(*selected_mode, SortingMode::ByTotalSubmissions)}>{"По общему количеству посылок от пользователя"}</option>
+                        <option value="ok-submissions" selected={matches!(*selected_mode, SortingMode::ByOkSubmissions)}>{"По количеству успешных посылок от пользователя"}</option>
+                        <option value="total-tasks" selected={matches!(*selected_mode, SortingMode::ByTotalTasks)}>{"По общему количеству задач, к которым пользователь приступил"}</option>
+                        <option value="ok-tasks" selected={matches!(*selected_mode, SortingMode::ByOkTasks)}>{"По количеству успешно решенных задач от пользователя"}</option>
+                        <option value="submission-date" selected={matches!(*selected_mode, SortingMode::BySubmissionDate)}>{"По дате посылки..."}</option>
+                        <option value="node-count" selected={matches!(*selected_mode, SortingMode::ByNodeCount)}>{"По количеству кружочков..."}</option>
+                        <option value="link-count" selected={matches!(*selected_mode, SortingMode::ByLinkCount)}>{"По количеству стрелочек..."}</option>
+                    </select>
+                );
+                let mut inner_select_values = vec![];
+                for (id, task) in leaderboard.tasks.iter().enumerate() {
+                    inner_select_values.push(
+                        html!(
+                            <option value={format!("{id}")} selected={*selected_task == id}>{id+1}{". "}{&task.name}</option>
+                        )
+                    );
+                }
+                let inner_select = if selected_mode.is_for_task() {
+                    let on_inner_select = {
+                        shadow_clone!(selected_task);
+                        Callback::from(move |ev: InputEvent| {
+                            let el: web_sys::HtmlSelectElement = ev.target_dyn_into().unwrap();
+                            let num: usize = el.value().parse().unwrap_or(0);
+                            selected_task.set(num);
+                        })
+                    };
+                    html!(
+                    <>
+                        <i>{" для задачи: "}</i>
+                        <select oninput={on_inner_select}>
+                            {for  inner_select_values}
+                        </select>
+                    </>
+                )} else {html!()};
+
+                html!(
+                    <>
+                    {outer_select}
+                    {inner_select}
+                    </>
+                )
+            };
 
             html!(
                 <>
                     <h1>{&leaderboard.name}</h1>
                     <p>{&leaderboard.legend}</p>
+                    <p><i>{"Сортировать: "}</i>{sorting_options}</p>
                     <table class="table">
                         <thead>
                             <tr>
